6351ea110c21a11c1e486d96a998987cd1e1ad14
Nish Lalgee <lalgee.nish@gmail.com>
2019-01-01 14:49:46 -0500

    Create API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  # Namespace API and version
+  namespace :api do
+    namespace :v1 do
+      resources :products do
+        resources :reviews
+      end
+    end
+  end
   #get 'products/index'
   resources :products, only: [:index]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
