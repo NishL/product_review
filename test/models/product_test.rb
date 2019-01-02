c654f9deec87eee392ac9eea5f1ba221cb1bf015
Nish Lalgee <lalgee.nish@gmail.com>
2019-01-02 14:45:36 -0500

    Add some quick tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,18 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  fixtures :products
+=begin
+  test "product attributes should not be empty" do
+    product = products(:one)
+    assert product.invalid?
+    assert product.errors[:name].any?
+    assert product.errors[:price].any?
+  end
+=end
+  test "name should not be empty" do
+    product = Product.new(price: 19.99, active: true)
+    assert product.valid?
+  end
+  
 end
